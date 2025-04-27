1362596034d2c564fa1b0c7a1515f65b148e0778
Bruno Sousa <brunodsousa@outlook.com>
2025-04-26 21:01:55 -0300

    projeto finalizado

---
+++ src/main/java/br/com/brunosousa/aulajparepository/entities/User.java
@@ -0,0 +1,63 @@
+package br.com.brunosousa.aulajparepository.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Double salary;
+
+    public User() {}
+
+    public User(Long id, String name, String email, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public User setSalary(Double salary) {
+        this.salary = salary;
+        return this;
+    }
+}
