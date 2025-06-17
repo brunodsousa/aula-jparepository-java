a56897e803e1b1477d91af3978d6d207b153cc27
Bruno Sousa <brunodsousa@outlook.com>
2025-06-17 16:01:07 -0300

    add Lombok annotations to User entity

--- src/main/java/br/com/brunosousa/aulajparepository/entities/User.java
+++ src/main/java/br/com/brunosousa/aulajparepository/entities/User.java
@@ -5,9 +5,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Entity
 @Table(name = "tb_users")
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,49 +25,4 @@ public class User {
     private String name;
     private String email;
     private Double salary;
-
-    public User() {}
-
-    public User(Long id, String name, String email, Double salary) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.salary = salary;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public User setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public User setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public User setSalary(Double salary) {
-        this.salary = salary;
-        return this;
-    }
 }
